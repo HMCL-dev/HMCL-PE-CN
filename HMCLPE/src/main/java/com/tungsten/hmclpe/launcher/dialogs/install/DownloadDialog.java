17d3c1fdbca8b689c15d70dbe3577671132df5ac
Tungstend <hanjinlong197@gmail.com>
2021-10-26 21:41:17 +0800

    Create DownloadDialog.java
    ...

---
+++ HMCLPE/src/main/java/com/tungsten/hmclpe/launcher/dialogs/install/DownloadDialog.java
@@ -0,0 +1,12 @@
+package com.tungsten.hmclpe.launcher.dialogs.install;
+
+import android.app.Dialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public class DownloadDialog extends Dialog {
+    public DownloadDialog(@NonNull Context context) {
+        super(context);
+    }
+}
