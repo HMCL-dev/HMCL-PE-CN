c2f3d3e5fc091a9ff077b2b9e8818f6ac6764475
Tungstend <hanjinlong197@gmail.com>
2021-12-01 01:15:52 +0800

    ...

--- HMCLPE/src/main/java/com/tungsten/hmclpe/launcher/uis/universal/setting/right/help/DonateUI.java
+++ HMCLPE/src/main/java/com/tungsten/hmclpe/launcher/uis/universal/setting/right/help/DonateUI.java
@@ -2,16 +2,22 @@ package com.tungsten.hmclpe.launcher.uis.universal.setting.right.help;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.tungsten.hmclpe.R;
 import com.tungsten.hmclpe.launcher.MainActivity;
 import com.tungsten.hmclpe.launcher.uis.tools.BaseUI;
 import com.tungsten.hmclpe.utils.animation.CustomAnimationUtils;
 
-public class DonateUI extends BaseUI {
+public class DonateUI extends BaseUI implements View.OnClickListener {
 
     public LinearLayout donateUI;
+
+    private TextView textView;
 
     public DonateUI(Context context, MainActivity activity) {
         super(context, activity);
@@ -21,6 +27,9 @@ public class DonateUI extends BaseUI {
     public void onCreate() {
         super.onCreate();
         donateUI = activity.findViewById(R.id.ui_donate);
+
+        textView = activity.findViewById(R.id.donate);
+        textView.setOnClickListener(this);
     }
 
     @SuppressLint("UseCompatLoadingForDrawables")
@@ -43,4 +52,12 @@ public class DonateUI extends BaseUI {
         }
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == textView){
+            Uri uri = Uri.parse("https://afdian.net/@tungs");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            context.startActivity(intent);
+        }
+    }
 }
--- HMCLPE/src/main/java/com/tungsten/hmclpe/launcher/uis/universal/setting/right/help/FeedbackUI.java
+++ HMCLPE/src/main/java/com/tungsten/hmclpe/launcher/uis/universal/setting/right/help/FeedbackUI.java
@@ -2,6 +2,10 @@ package com.tungsten.hmclpe.launcher.uis.universal.setting.right.help;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.tungsten.hmclpe.R;
@@ -9,9 +13,12 @@ import com.tungsten.hmclpe.launcher.MainActivity;
 import com.tungsten.hmclpe.launcher.uis.tools.BaseUI;
 import com.tungsten.hmclpe.utils.animation.CustomAnimationUtils;
 
-public class FeedbackUI extends BaseUI {
+public class FeedbackUI extends BaseUI implements View.OnClickListener {
 
     public LinearLayout feedbackUI;
+
+    private ImageButton joinQQ;
+    private ImageButton jumpToGit;
 
     public FeedbackUI(Context context, MainActivity activity) {
         super(context, activity);
@@ -21,6 +28,12 @@ public class FeedbackUI extends BaseUI {
     public void onCreate() {
         super.onCreate();
         feedbackUI = activity.findViewById(R.id.ui_feedback);
+
+        joinQQ = activity.findViewById(R.id.join_qq_group);
+        jumpToGit = activity.findViewById(R.id.jump_to_git_issues);
+
+        joinQQ.setOnClickListener(this);
+        jumpToGit.setOnClickListener(this);
     }
 
     @SuppressLint("UseCompatLoadingForDrawables")
@@ -43,4 +56,36 @@ public class FeedbackUI extends BaseUI {
         }
     }
 
+    /****************
+     *
+     * 发起添加群流程。群号：HMCL-PE(715191324) 的 key 为： 7rX0cr37hu_jNPaGIlqAEf4Ndv1BG-WU
+     * 调用 joinQQGroup(7rX0cr37hu_jNPaGIlqAEf4Ndv1BG-WU) 即可发起手Q客户端申请加群 HMCL-PE(715191324)
+     *
+     * @param key 由官网生成的key
+     * @return 返回true表示呼起手Q成功，返回false表示呼起失败
+     ******************/
+    public boolean joinQQGroup(String key) {
+        Intent intent = new Intent();
+        intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26jump_from%3Dwebapi%26k%3D" + key));
+        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        try {
+            context.startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            // 未安装手Q或安装的版本不支持
+            return false;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == joinQQ){
+            joinQQGroup("7rX0cr37hu_jNPaGIlqAEf4Ndv1BG-WU");
+        }
+        if (v == jumpToGit){
+            Uri uri = Uri.parse("https://github.com/Tungstend/HMCL-PE/issues");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            context.startActivity(intent);
+        }
+    }
 }
