cc7def93b451dcdfc1d73806087b63efd56b5b8d
Tungstend <hanjinlong197@gmail.com>
2022-02-23 22:23:34 +0800

    操

---
+++ HMCLPE/src/main/java/com/tungsten/hmclpe/launcher/dialogs/control/AddViewDialog.java
@@ -0,0 +1,14 @@
+package com.tungsten.hmclpe.launcher.dialogs.control;
+
+import android.app.Dialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public class AddViewDialog extends Dialog {
+
+    public AddViewDialog(@NonNull Context context) {
+        super(context);
+    }
+
+}
