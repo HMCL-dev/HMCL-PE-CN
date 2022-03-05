15764c2bec0502ec21d9e72f136c67b4c43c023d
Tungstend <hanjinlong197@gmail.com>
2022-03-05 22:09:09 +0800

    add nothing

---
+++ HMCLPE/src/main/java/com/tungsten/hmclpe/launcher/dialogs/control/SelectKeycodeDialog.java
@@ -0,0 +1,16 @@
+package com.tungsten.hmclpe.launcher.dialogs.control;
+
+import android.app.Dialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public class SelectKeycodeDialog extends Dialog {
+
+
+
+    public SelectKeycodeDialog(@NonNull Context context) {
+        super(context);
+    }
+
+}
