256f123b81d816abc2098c67779f16647a651f39
ShirosakiMio <852468399@qq.com>
2022-04-05 19:05:09 +0800

    增加蓝奏云链接获取
    bug++

---
+++ HMCLPE/src/main/java/com/tungsten/hmclpe/task/LanzouUrlGetTask.java
@@ -0,0 +1,86 @@
+package com.tungsten.hmclpe.task;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.lang.ref.WeakReference;
+
+public class LanzouUrlGetTask extends AsyncTask<String, Integer, String> {
+    public interface Callback{
+        void onStart();
+        void onFinish(String url);
+    }
+    private WeakReference<Context> ctx;
+    private Callback callback;
+    public LanzouUrlGetTask(Context ctx,Callback callback) {
+        this.ctx=new WeakReference<>(ctx);
+        this.callback=callback;
+    }
+
+
+    @Override
+    public void onPreExecute() {
+        ((Activity)ctx.get()).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                callback.onStart();
+            }
+        });
+    }
+
+    @Override
+    public String doInBackground(String... args) {
+        try {
+            String url = args[0];
+            Document doc= null;
+            doc = Jsoup.connect(url)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36")
+                    .get();
+            Elements elements = doc.getElementsByClass("ifr2");
+            for (Element element:elements){
+                url=url.substring(0,url.indexOf(".com")+4)+element.attr("src");
+                WebClient webClient=new WebClient();
+                HtmlPage page=webClient.getPage(url);
+                webClient.getOptions().setJavaScriptEnabled(true);
+                webClient.getOptions().setCssEnabled(false);
+                webClient.getOptions().setRedirectEnabled(true);
+                webClient.getOptions().setThrowExceptionOnScriptError(false);
+                webClient.getOptions().setTimeout(50000);
+                webClient.waitForBackgroundJavaScript(5*1000);
+                doc=Jsoup.parse(page.asXml());
+                Elements elements1 = doc.getElementsByTag("a");
+                return elements1.get(0).attr("href");
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... p1) {
+
+    }
+
+    @Override
+    public void onPostExecute(String result) {
+        ((Activity)ctx.get()).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                callback.onFinish(result);
+            }
+        });
+    }
+
+}
