5ed22c9780d3b7404f78426596e51efca3019bdd
ShirosakiMio <852468399@qq.com>
2022-04-06 10:23:54 +0800

    Update LanzouUrlGetTask.java

--- HMCLPE/src/main/java/com/tungsten/hmclpe/task/LanzouUrlGetTask.java
+++ HMCLPE/src/main/java/com/tungsten/hmclpe/task/LanzouUrlGetTask.java
@@ -3,7 +3,13 @@ package com.tungsten.hmclpe.task;
 import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
+import android.webkit.DownloadListener;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
@@ -14,6 +20,9 @@ import org.jsoup.select.Elements;
 
 import java.lang.ref.WeakReference;
 
+/**
+ * @author ShirosakiMio
+ */
 public class LanzouUrlGetTask extends AsyncTask<String, Integer, String> {
     public interface Callback{
         void onStart();
@@ -21,6 +30,8 @@ public class LanzouUrlGetTask extends AsyncTask<String, Integer, String> {
     }
     private WeakReference<Context> ctx;
     private Callback callback;
+    private String fianalUrl=null;
+    private WebView web;
     public LanzouUrlGetTask(Context ctx,Callback callback) {
         this.ctx=new WeakReference<>(ctx);
         this.callback=callback;
@@ -48,22 +59,69 @@ public class LanzouUrlGetTask extends AsyncTask<String, Integer, String> {
             Elements elements = doc.getElementsByClass("ifr2");
             for (Element element:elements){
                 url=url.substring(0,url.indexOf(".com")+4)+element.attr("src");
-                WebClient webClient=new WebClient();
-                HtmlPage page=webClient.getPage(url);
+                WebClient webClient=new WebClient(BrowserVersion.CHROME);
+                HtmlPage page;
+                page=webClient.getPage(url);
                 webClient.getOptions().setJavaScriptEnabled(true);
                 webClient.getOptions().setCssEnabled(false);
                 webClient.getOptions().setRedirectEnabled(true);
                 webClient.getOptions().setThrowExceptionOnScriptError(false);
                 webClient.getOptions().setTimeout(50000);
                 webClient.waitForBackgroundJavaScript(5*1000);
+                for (int i = 0; i < 20; i++) {
+                    if (page != null) {
+//                        System.out.println("等待ajax执行完毕");
+                        break;
+                    }
+                    synchronized (page) {
+                        page.wait(500);
+                    }
+                }
                 doc=Jsoup.parse(page.asXml());
                 Elements elements1 = doc.getElementsByTag("a");
-                return elements1.get(0).attr("href");
+                url=elements1.get(0).attr("href");
+                String finalUrl = url;
+                ((Activity)ctx.get()).runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        web=new WebView(ctx.get());
+                        web.getSettings().setJavaScriptEnabled(true);
+                        web.getSettings().setUserAgentString("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0");
+                        web.setWebViewClient(new WebViewClient(){
+                            @Override
+                            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                                // 限制使用内置浏览器
+                                view.loadUrl(request.getUrl().toString());
+                                return true;
+                            }
 
+                            @Override
+                            public void onPageFinished(WebView view, String url) {
+                                super.onPageFinished(view, url);
+                            }
+                        });
+                        web.setDownloadListener(new DownloadListener(){
+                            @Override
+                            public void onDownloadStart(String url, String userAgent, String contentDisposition,String mimetype, long contentLength) {
+                                fianalUrl=url;
+                                web.clearCache(true);
+                                web.clearCache(true);
+                                web=null;
+                            }
+                        });
+                        web.loadUrl(finalUrl);
+                    }
+                });
+                while (fianalUrl==null){
+
+                }
+                webClient.closeAllWindows();
+                return fianalUrl;
             }
 
         } catch (Exception e) {
             e.printStackTrace();
+            Log.e("测试",e.toString());
         }
         return null;
     }
