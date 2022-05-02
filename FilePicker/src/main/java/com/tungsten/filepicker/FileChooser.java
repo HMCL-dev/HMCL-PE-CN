c9d8773efc245c63aed70b99232b9e307c557cf3
Tungstend <hanjinlong197@gmail.com>
2022-05-02 22:40:44 +0800

    fix filepicker

--- FilePicker/src/main/java/com/tungsten/filepicker/FileChooser.java
+++ FilePicker/src/main/java/com/tungsten/filepicker/FileChooser.java
@@ -122,7 +122,7 @@ public class FileChooser extends AppCompatActivity implements OnFileChangedListe
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == APP_PERMISSION_REQUEST ) {
-            if (resultCode != Activity.RESULT_OK)
+            //if (resultCode != Activity.RESULT_OK)
                 //Toast.makeText(mContext,mContext.getString(R.string.permission_error),Toast.LENGTH_LONG).show();
             loadUi();
         }
