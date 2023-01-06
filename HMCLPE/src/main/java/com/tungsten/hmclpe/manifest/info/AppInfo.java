55506284c77004eff62d87b38273da3a617e0f9e
zkitefly <64117916+zkitefly@users.noreply.github.com>
2023-01-06 14:11:16 +0800

    Update AppInfo.java

--- HMCLPE/src/main/java/com/tungsten/hmclpe/manifest/info/AppInfo.java
+++ HMCLPE/src/main/java/com/tungsten/hmclpe/manifest/info/AppInfo.java
@@ -7,9 +7,9 @@ public class AppInfo {
     public final static int CONTROL_VERSION_CODE = 1;
     public final static int JAVA_17_VERSION_CODE = 2;
     public final static String JAVA_17_DOWNLOAD_URL = "https://wwc.lanzoub.com/iOpx406bug9i";
-    public final static String JAVA_17_DOWNLOAD_URL_GITHUB = "https://github.com/Tungstend/HMCL-PE/releases/download/Java17-noCert/JRE17.zip";
-    public final static String JAVA_17_DOWNLOAD_URL_FASTGIT = "https://gitee.com/Tungstend/HMCL-PE/attach_files/1093114/download/JRE17.zip";
-    public final static String JAVA_17_DOWNLOAD_URL_GITCODE = "https://gitcode.net/ShirosakiMio/hmcl-pe-runtime/-/raw/master/JRE17.zip?inline=false";
+    public final static String JAVA_17_DOWNLOAD_URL_GITHUB = "https://github.com/zkitefly/zkitefly.github.io/releases/download/0/JRE17.zip";
+    public final static String JAVA_17_DOWNLOAD_URL_FASTGIT = "https://gitee.com/bleaker/zkitefly.github.io/releases/download/0/JRE17.zip";
+    public final static String JAVA_17_DOWNLOAD_URL_GITCODE = "https://gitcode.net/ShirosakiMio/hmcl-pe-runtime/-/raw/master/JRE17.zip";
     public final static String JAVA_17_SHA1 = "3b89732c972d1b4907447103c9dd7c819260e0d4";
     public final static boolean IS_PUBLIC_VERSION = true;
 
